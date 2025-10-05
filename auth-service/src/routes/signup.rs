59f27896b80cc6bf6fe244810d806e09bea8eb8c
Hritique Rungta <hritique@chorus.one>
2025-10-06 00:02:59 +0400

    [sprint 1][task 8] feat: implement signup interface

--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,6 +1,16 @@
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
+use axum::Json;
+use serde::Deserialize;
 
-pub async fn signup() -> impl IntoResponse {
+#[derive(Deserialize)]
+pub struct SignupRequest {
+    email: String,
+    password: String,
+    #[serde(rename = "require2FA")]
+    require_2fa: bool,
+}
+
+pub async fn signup(Json(_): Json<SignupRequest>) -> impl IntoResponse {
     StatusCode::OK.into_response()
 }
--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,10 +1,29 @@
-use crate::helpers::TestApp;
+use crate::helpers::{get_random_email, TestApp};
 
 #[tokio::test]
-async fn signup_returns_200() {
+async fn signup_returns_422_for_invalid_body() {
     let app = TestApp::new().await;
 
-    let response = app.post_signup().await;
+    let random_email = get_random_email();
 
-    assert_eq!(response.status().as_u16(), 200);
+    let test_cases = vec![
+        serde_json::json!({
+            "email": random_email,
+            "password": "password",
+        }),
+        serde_json::json!({
+            "password": "password",
+            "require2FA": false
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "require2FA": true
+        }),
+    ];
+
+    for test_case in test_cases {
+        let response = app.post_signup(&test_case).await;
+
+        assert_eq!(response.status().as_u16(), 422);
+    }
 }
